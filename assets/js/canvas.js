6498efba21c107b81a11cef7c4be76f624e47db1
Abendiix <55549785+Abendiix@users.noreply.github.com>
2022-07-31 12:14:57 +0200

    Update canvas.js

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -42,7 +42,7 @@ function onLoad()
     controlchange.style.display = "none";
     controlremove.style.display = "none";
     
-    folder = "https://github.com/Abendiix/spriteslove/blob/main/assets/img/images/";
+    folder = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/images/"
 }
 
 function fn() {
@@ -235,7 +235,7 @@ function flipped()
 function blue_blue()
 {
     borderimg = document.getElementById('blue_blue');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/blue_blue.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/blue_blue.png";
     ndsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -248,7 +248,7 @@ function blue_blue()
 function blue_pink()
 {
     borderimg = document.getElementById('blue_pink');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/blue_pink.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/blue_pink.png";
     ndsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -261,7 +261,7 @@ function blue_pink()
 function blue_green()
 {
     borderimg = document.getElementById('blue_green');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/blue_green.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/blue_green.png";
     ndsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -274,7 +274,7 @@ function blue_green()
 function dt_blue()
 {
     borderimg = document.getElementById('dt_blue');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/dt_blue.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/dt_blue.png";
     ndsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -287,7 +287,7 @@ function dt_blue()
 function dt_pink()
 {
     borderimg = document.getElementById('dt_pink');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/dt_pink.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/dt_pink.png";
     ndsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -300,7 +300,7 @@ function dt_pink()
 function dt_green()
 {
     borderimg = document.getElementById('dt_green');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/dt_green.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/dt_green.png";
     ndsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -313,7 +313,7 @@ function dt_green()
 function sky_blue()
 {
     borderimg = document.getElementById('sky_blue');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/sky_blue.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/sky_blue.png";
     ndsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -326,7 +326,7 @@ function sky_blue()
 function sky_pink()
 {
     borderimg = document.getElementById('sky_pink');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/sky_pink.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/sky_pink.png";
     ndsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -339,7 +339,7 @@ function sky_pink()
 function sky_green()
 {
     borderimg = document.getElementById('sky_green');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/sky_green.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/sky_green.png";
     ndsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -352,7 +352,7 @@ function sky_green()
 function mega()
 {
     borderimg = document.getElementById('mega');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/mega.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/mega.png";
     n3dsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -365,7 +365,7 @@ function mega()
 function gates()
 {
     borderimg = document.getElementById('gates');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/gates.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/gates.png";
     n3dsrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
@@ -378,7 +378,7 @@ function gates()
 function dx()
 {
     borderimg = document.getElementById('dx');
-    borderimg.src = "https://github.com/Abendiix/spriteslove/tree/main/assets/img/borders/dx.png";
+    borderimg.src = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/borders/dx.png";
     nswitchrow.style.display = "none";
     control.style.display = "flex";
     controladd.style.display = "none";
