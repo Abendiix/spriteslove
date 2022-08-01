c2d4f1a7987a83b968a9b490fe4830507267d9f0
Abendiix <55549785+Abendiix@users.noreply.github.com>
2022-08-01 23:05:58 +0200

    Update canvas.js

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -44,7 +44,7 @@ function onLoad()
     controlchange.style.display = "none";
     controlremove.style.display = "none";
     
-    folder = "a";
+    folder = "https://raw.githubusercontent.com/Abendiix/spriteslove/main/assets/img/images/";
 }
 
 function fn() {
